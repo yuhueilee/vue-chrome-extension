67370f49b7b36fbae183f3885355fb56f076f654
yuhueilee <wendylee70127@gmail.com>
2024-03-22 14:54:37 +0800

    Reinstall dependencies packages for laravel-mix, update script, rename folder

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,6 +4,6 @@ let mix = require("laravel-mix");
 // 2. Compile scss file to be stored under 'dist/css' directory.
 // 3. Prevent webpack from automatically process/optimize relative stylesheet url()'s.
 // learn more: https://laravel-mix.com/docs/5.0/options
-mix.setPublicPath("./").sass("assets/sass/popup.scss", "dist/css/").options({
+mix.setPublicPath("dist").sass("assets/sass/popup.scss", "dist/css/").options({
     processCssUrls: false,
 });
